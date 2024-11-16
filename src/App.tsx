607fb33fa04e94b0b5a0b7e3555bb54ea068f2f5
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2024-11-16 16:56:35 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Heading, VStack } from '@chakra-ui/react'
+
+
+function App() {
+
+
+  return (
+    <VStack>
+      <Heading>Hello</Heading>
+    </VStack>
+  )
+}
+
+export default App
